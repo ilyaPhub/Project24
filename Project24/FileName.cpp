7c3e4bcc23d78d88ebe5f9297992691083b2d8c5
Илья Плисюк < @DESKTOP-EJHTIJ2>
2024-10-14 21:56:31 +0300

    Add project files.

---
+++ Project24/FileName.cpp
@@ -0,0 +1,47 @@
+#include "GPU.h"
+GPU::GPU(const char* name, int memorySize) : memorySize(memorySize)
+{
+    int i = 0;
+    while (name[i] != '\0') ++i;
+    this->name = new char[i + 1];
+
+    for (int j = 0; j < i; ++j)
+    {
+        this->name[j] = name[j];
+    }
+    this->name[i] = '\0';
+}
+
+GPU::~GPU()
+{
+    delete[] name;
+}
+
+const char* GPU::getName() const
+{
+    return name;
+}
+
+int GPU::getMemorySize() const
+{
+    return memorySize;
+}
+
+void GPU::setName(const char* name)
+{
+    delete[] this->name;
+    int i = 0;
+    while (name[i] != '\0') ++i;
+    this->name = new char[i + 1];
+
+    for (int j = 0; j < i; ++j)
+    {
+        this->name[j] = name[j];
+    }
+    this->name[i] = '\0';
+}
+
+void GPU::setMemorySize(int memorySize)
+{
+    this->memorySize = memorySize;
+}
